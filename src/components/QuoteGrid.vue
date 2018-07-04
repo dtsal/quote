04105b3bf9dab35374199ef957c99b1f8407b3dd
dtsal <tsal.dmitry@gmail.com>
2018-07-04 17:27:08 +0300

    Initial commit

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="row">
+
+   </div>
+</template>
+
+<script>
+    export default {
+      props: ['quotes']
+    }
+</script>
+
+<style scoped>
+   .panel-body {
+     font-family: 'Arizonia',cursive;
+     font-size: 24px;
+     color: #6e6e6e;
+   }
+  .quote {
+    cursor: pointer;
+  }
+  .quote:hover {
+    background-color: #ffe2e2;
+  }
+</style>
