2b602dbaaf27b2c010823cb2723a45aef34fe4cb
dtsal <tsal.dmitry@gmail.com>
2018-07-06 18:42:49 +0300

    added new Quote and Quote Delete methods

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="container">
-      <app-quote-grid :quotes="quotes" >
-
-      </app-quote-grid>
+      <app-new-quote v-on:quoteAdded="addNewQuote"></app-new-quote>
+      <app-quote-grid :quotess="quotes" :maxQuotes="maxQuotes" @quoteDeleted="deleteTheQuote"></app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info: Click on a Quote to delete it</div>
+        </div>
+      </div>
   </div>
 </template>
 
 
 <script>
+  import NewQuote from './components/NewQuote';
   import QuoteGrid from './components/QuoteGrid.vue';
   export default {
     data: function() {
       return {
-        quotes: ['Just a quote to see something','Just another quote'],
+        quotes: ['Just a quote to see something'],
         maxQuotes: 10
       }
     },
     components: {
-      appQuoteGrid: QuoteGrid
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    },
+    methods: {
+      addNewQuote(quote) {
+          this.quotes.push(quote)
+      },
+      deleteTheQuote(index) {
+         this.quotes.splice(index,1);
+      }
     }
 
   }
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,15 +1,20 @@
 <template>
    <div class="row">
-         <app-quote v-for="quote in quotes">{{quote}}</app-quote>
+         <app-quote v-for="(quote,index) in quotess" @click.native="deleteQuote(index)">{{quote}}{{maxQuotes}}</app-quote>
    </div>
 </template>
 
 <script>
     import Quote from './Quote.vue';
     export default {
-      props: ['quotes'],
+      props: ['quotess','maxQuotes'],
       components: {
          appQuote: Quote
+      },
+      methods: {
+        deleteQuote(index) {
+          this.$emit('quoteDeleted',index)
+        }
       }
     }
 </script>
